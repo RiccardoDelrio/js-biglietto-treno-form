0a2aaa3f43d04af7df52f0a7f1bd489330e230ec
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-19 17:01:28 +0100

    entered the values ​​inside the ticket

--- main.js
+++ main.js
@@ -2,34 +2,40 @@ const kilometriEl = document.querySelector("#number-of-km")
 const formEL = document.querySelector(".form")
 const btnEl = document.querySelector(".calculator")
 const etaEl = document.querySelector(".selettore")
-console.log(kilometriEl, etaEl);
 const userName = document.querySelector("#name")
-const namePassangers = document.querySelector(".name-passengers")
+const namePassangers = document.querySelector("#name-passengers")
 const km = kilometriEl.value
-
+const finalPrice = document.querySelector("#price")
+const typeOf = document.querySelector("#typeOf")
+console.log(kilometriEl, etaEl);
 
 
 
 
 formEL.addEventListener("submit", function (e) {
     e.preventDefault()
+    namePassangers.innerHTML = (`${userName.value}`)
+
     const km = kilometriEl.value
     let eta = etaEl.value
     console.log(eta);
     if (eta === "1") {
         eta = 17
+        typeOf.innerHTML = ("Biglietto Ridotto <18")
     } else if (eta === "2") {
         eta = 33
+        typeOf.innerHTML = ("Biglietto ordinario")
     } else {
         eta = 66
+        typeOf.innerHTML = ("Biglietto Ridotto >65")
+
     }
     console.log(eta);
     const prezzoLordo = km * 0.21;
     findPrice(eta, prezzoLordo)
-    namePassangers.innerHTML = userName
-    result.innerHTML = (`${km} ${eta} ${findPrice(eta, prezzoLordo)}`)
-
+    finalPrice.innerHTML = (`${findPrice(eta, prezzoLordo)} €`)
 })
+
 /**
  * 
  * @param {age} num 
@@ -48,6 +54,7 @@ const findPrice = (num, num2) => {
         console.log(sconto);
         const prezzoNetto = (num2 - sconto);
         console.log(prezzoNetto.toFixed(2))
+
         return prezzoNetto.toFixed(2)
     } else {
         console.log(num2.toFixed(2));
